4e2276dab5464f89b21d6b217a4381bbf420f76a
no_gen_mu <liujianhishen@gmail.com>
2026-03-11 20:36:40 +0800

    238

---
+++ 专题/前缀积/238-除自身以外数组的乘积/238.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [1] * n
+        for i in range(1,n):
+            answer[i] = answer[i-1] * nums[i-1]
+        right = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+        return answer
+
+
+
+s = Solution()
+print(s.productExceptSelf([1,2,3,4]))
